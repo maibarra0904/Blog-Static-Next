6c8a4eda1f12b95acca03ff385db1f7340d26cda
maibarra0904 <mario.ibarra.86@gmail.com>
2023-11-10 09:49:11 -0500

    Primer lanzamiento en Next

---
+++ _next/static/chunks/app/not-found-11e24b2d10df4cdf.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160],{5567:function(e,t,r){Promise.resolve().then(r.bind(r,3619))},3619:function(e,t,r){"use strict";r.r(t);var n=r(7437);r(2265);var o=r(4033),s=r(2674);t.default=()=>((0,o.useRouter)(),(0,n.jsxs)("div",{className:"grid gap-4 place-content-center",children:[(0,n.jsx)("h1",{className:"text-center text-2xl",children:"404"}),(0,n.jsx)("h1",{className:"text-center text-2xl",children:"Page Not Found"}),(0,n.jsx)("div",{className:"text-center",children:(0,n.jsx)(s.default,{text:"rounded bg-gradient-to-r from-blue-500 hover:to-yellow-400 px-4 py-2 text-white hover:from-pink-500 to-yellow-500",children:"Volver"})})]}))},2674:function(e,t,r){"use strict";r.r(t);var n=r(7437);r(2265);var o=r(4033);t.default=e=>{let{children:t,text:r=""}=e,s=(0,o.useRouter)();return(0,n.jsx)("button",{onClick:()=>s.back(),type:"button",className:r,children:t})}},622:function(e,t,r){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=r(2265),o=Symbol.for("react.element"),s=Symbol.for("react.fragment"),c=Object.prototype.hasOwnProperty,l=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,u={key:!0,ref:!0,__self:!0,__source:!0};function q(e,t,r){var n,s={},a=null,i=null;for(n in void 0!==r&&(a=""+r),void 0!==t.key&&(a=""+t.key),void 0!==t.ref&&(i=t.ref),t)c.call(t,n)&&!u.hasOwnProperty(n)&&(s[n]=t[n]);if(e&&e.defaultProps)for(n in t=e.defaultProps)void 0===s[n]&&(s[n]=t[n]);return{$$typeof:o,type:e,key:a,ref:i,props:s,_owner:l.current}}t.Fragment=s,t.jsx=q,t.jsxs=q},7437:function(e,t,r){"use strict";e.exports=r(622)},4033:function(e,t,r){e.exports=r(94)}},function(e){e.O(0,[971,472,744],function(){return e(e.s=5567)}),_N_E=e.O()}]);
